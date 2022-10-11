4e01007df55c558061906c28e0d39f405b16b300
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-11 17:47:01 +0100

    Initialise GaussianProcess with measurement noise, and apply to the covariance matrix when sampling

--- Course 1 - Data, Estimation and Inference/gp.py
+++ Course 1 - Data, Estimation and Inference/gp.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 class GaussianProcess:
-    def __init__(self, prior_mean_func, kernel_func, rng=None):
+    def __init__(self, prior_mean_func, kernel_func, noise_std, rng=None):
         self._prior_mean_func = prior_mean_func
         self._kernel_func = kernel_func
+        self._noise_var = noise_std * noise_std
 
         if rng is None:
             rng = np.random.default_rng()
@@ -12,8 +13,8 @@ class GaussianProcess:
 
     def sample_prior(self, x):
         mean = self._prior_mean_func(x)
-        cov = self._kernel_func(x, x)
+        cov = self._kernel_func(x, x) + self._noise_var * np.identity(x.size)
         root_cov = np.linalg.cholesky(cov)
-        noise = self._rng.normal(size=x.shape)
-        samples = root_cov @ noise + mean
+        pre_transform_samples = self._rng.normal(size=x.shape)
+        samples = root_cov @ pre_transform_samples + mean
         return samples
