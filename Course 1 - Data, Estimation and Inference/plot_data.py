d8d4696143274c52bc6a79108682133df2789014
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-11 17:48:39 +0100

    Plot samples from the GP prior

--- Course 1 - Data, Estimation and Inference/plot_data.py
+++ Course 1 - Data, Estimation and Inference/plot_data.py
@@ -1,9 +1,13 @@
 import time
 import calendar
+import numpy as np
 import __init__
 import util
 import plotting
 import data
+import gp
+import mean
+import kernel
 
 data_dict, num_columns, num_rows = data.load_dict()
 assert num_columns == 19
@@ -38,4 +42,15 @@ pred_lines = [plotting.HVLine(v=t, c="r", alpha=0.2) for t in t_pred]
 plotting.plot(
     [data_line] + pred_lines,
     "Tide height (m) vs time (days), including missing data points"
+)
+
+g = gp.GaussianProcess(mean.ZeroMean(), kernel.SquaredExponential(1, 1), 0.1)
+num_prior_samples = 5
+prior_sample_lines = [
+    plotting.Line(t_list, g.sample_prior(np.array(t_list)), c="b", alpha=0.5)
+    for _ in range(num_prior_samples)
+]
+plotting.plot(
+    [data_line] + prior_sample_lines,
+    "Samples from GP prior vs data",
 )
