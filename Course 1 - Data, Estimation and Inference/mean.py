a766242c6cefe7eb66ce76466231bf4d77b283ed
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-13 13:35:42 +0100

    Add Constant mean function

--- Course 1 - Data, Estimation and Inference/mean.py
+++ Course 1 - Data, Estimation and Inference/mean.py
@@ -4,6 +4,13 @@ class ZeroMean:
     def __call__(self, x):
         return np.zeros(x.shape)
 
+class Constant:
+    def __init__(self, offset):
+        self._offset = offset
+
+    def __call__(self, x):
+        return np.full(x.shape, self._offset)
+
 class Linear:
     def __init__(self, scale, offset):
         self._scale = scale
