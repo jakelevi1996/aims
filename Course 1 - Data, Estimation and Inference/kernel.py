eecb127c0d0238f90a34ba1b8310cc32d6ad35a7
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-13 15:58:33 +0100

    Add linear kernel

--- Course 1 - Data, Estimation and Inference/kernel.py
+++ Course 1 - Data, Estimation and Inference/kernel.py
@@ -9,3 +9,12 @@ class SquaredExponential:
         sq_distance = np.square((x1 - x2) / self._length_scale)
         k = self._kernel_scale * np.exp(-sq_distance)
         return k
+
+class Linear:
+    def __init__(self, length_scale, centre):
+        self._sq_length_scale = length_scale * length_scale
+        self._centre = centre
+
+    def __call__(self, x1, x2):
+        k = (x1 - self._centre) * (x2 - self._centre) / self._sq_length_scale
+        return k
