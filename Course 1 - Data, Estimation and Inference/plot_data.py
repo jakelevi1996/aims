a41aa559f1732cdae0d53dcc4721f6a6b80e6937
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-13 19:14:13 +0100

    Make plot of independent GP predictions

--- Course 1 - Data, Estimation and Inference/plot_data.py
+++ Course 1 - Data, Estimation and Inference/plot_data.py
@@ -22,3 +22,45 @@ plotting.plot(
     ),
     plot_name="Tide height (m) vs time (days)",
 )
+
+y_pred_mean = data.parse_column(
+    data_dict,
+    "Independent tide height prediction (m)",
+)
+y_pred_std = data.parse_column(
+    data_dict,
+    "Independent tide height deviation (m)",
+)
+
+plotting.plot(
+    plotting.Line(
+        t_data,
+        y_data,
+        c="k",
+        ls="",
+        marker="o",
+        alpha=0.5,
+        zorder=20,
+    ),
+    plotting.Line(
+        t_pred,
+        data.parse_column(data_dict, "True tide height (m)"),
+        c="k",
+        ls="",
+        marker="x",
+        alpha=0.5,
+        zorder=20,
+    ),
+    plotting.Line(t_pred, y_pred_mean, c="r", zorder=40),
+    plotting.FillBetween(
+        t_pred,
+        y_pred_mean + 2*y_pred_std,
+        y_pred_mean - 2*y_pred_std,
+        color="r",
+        lw=0,
+        alpha=0.2,
+        zorder=30,
+    ),
+    plot_name="Data and independent GP predictions",
+    axis_properties=plotting.AxisProperties(ylim=[0, 6]),
+)
