a87df32cd1cb6905f71f95682cc7c9641bd0738d
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-16 19:49:40 +0100

    Find optimal parameters for kernel.Periodic

--- Course 1 - Data, Estimation and Inference/find_best_params.py
+++ Course 1 - Data, Estimation and Inference/find_best_params.py
@@ -9,31 +9,42 @@ import plotting
 sotonmet = data.Sotonmet()
 t_pred = np.linspace(-1, 6, 1000)
 
-g = gp.GaussianProcess(
-    prior_mean_func=mean.Constant(3),
-    kernel_func=kernel.SquaredExponential(0.3, 10),
-    noise_std=1,
-)
-g.optimise_hyperparameters(sotonmet.t_train, sotonmet.y_train)
-print(g)
-
-g.decondition()
-g.condition(sotonmet.t_train, sotonmet.y_train)
-y_pred_mean, y_pred_std = g.predict(t_pred)
-print(g.log_marginal_likelihood())
-
-plotting.plot(
-    *sotonmet.get_train_test_plot_lines(),
-    plotting.Line(t_pred, y_pred_mean, c="r", zorder=40),
-    plotting.FillBetween(
-        t_pred,
-        y_pred_mean + 2*y_pred_std,
-        y_pred_mean - 2*y_pred_std,
-        color="r",
-        lw=0,
-        alpha=0.2,
-        zorder=30,
+g_list = [
+    gp.GaussianProcess(
+        prior_mean_func=mean.Constant(3),
+        kernel_func=kernel.SquaredExponential(0.3, 10),
+        noise_std=1,
     ),
-    plot_name="Data and optimised GP predictions",
-    axis_properties=plotting.AxisProperties(ylim=[0, 6]),
-)
+    gp.GaussianProcess(
+        prior_mean_func=mean.Constant(3),
+        kernel_func=kernel.Periodic(0.5, 0.3, 10),
+        noise_std=1,
+    ),
+]
+
+for g in g_list:
+    g.optimise_hyperparameters(sotonmet.t_train, sotonmet.y_train)
+
+for g in g_list:
+    print(g)
+
+    g.decondition()
+    g.condition(sotonmet.t_train, sotonmet.y_train)
+    y_pred_mean, y_pred_std = g.predict(t_pred)
+    print(g.log_marginal_likelihood())
+
+    plotting.plot(
+        *sotonmet.get_train_test_plot_lines(),
+        plotting.Line(t_pred, y_pred_mean, c="r", zorder=40),
+        plotting.FillBetween(
+            t_pred,
+            y_pred_mean + 2*y_pred_std,
+            y_pred_mean - 2*y_pred_std,
+            color="r",
+            lw=0,
+            alpha=0.2,
+            zorder=30,
+        ),
+        plot_name="Data and optimised GP predictions, GP = %r" % g,
+        axis_properties=plotting.AxisProperties(ylim=[0, 6]),
+    )
