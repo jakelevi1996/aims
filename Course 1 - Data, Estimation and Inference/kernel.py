8c8f01eac588b2f48b5ae7325ad292f95419dd64
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-16 16:06:57 +0100

    Add Periodic kernel

--- Course 1 - Data, Estimation and Inference/kernel.py
+++ Course 1 - Data, Estimation and Inference/kernel.py
@@ -17,6 +17,30 @@ class SquaredExponential:
         )
         return s
 
+class Periodic:
+    def __init__(self, period, length_scale, kernel_scale):
+        self._angular_freq = 2 * np.pi / period
+        self._length_scale = length_scale
+        self._kernel_scale = kernel_scale
+
+    def __call__(self, x1, x2):
+        k = (
+            self._kernel_scale
+            * np.exp(
+                np.cos(self._angular_freq * (x1 - x2))
+                / self._length_scale
+            )
+        )
+        return k
+
+    def __repr__(self):
+        period = 2 * np.pi / self._angular_freq
+        s = (
+            "Periodic(period=%r, length_scale=%r, kernel_scale=%r)"
+            % (period, self._length_scale, self._kernel_scale)
+        )
+        return s
+
 class Linear:
     def __init__(self, length_scale, centre):
         self._sq_length_scale = length_scale * length_scale
