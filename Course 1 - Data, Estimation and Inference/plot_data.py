66ae243473403e97a61c4dabbb9b1d82e81172cc
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-16 12:23:18 +0100

    Update plot_data.py to use the new Sotonmet class interface

--- Course 1 - Data, Estimation and Inference/plot_data.py
+++ Course 1 - Data, Estimation and Inference/plot_data.py
@@ -2,58 +2,25 @@ import __init__
 import data
 import plotting
 
-data_dict, num_columns, num_rows = data.load_dict()
-assert num_columns == 19
-assert num_rows == 1258
-t_pred, t_data, y_data = data.parse_dict(data_dict)
-assert t_pred.size == 1258
-assert t_data.size == 917
-assert y_data.size == 917
+sotonmet = data.Sotonmet()
 
 plotting.plot(
-    plotting.Line(
-        t_data,
-        y_data,
-        c="k",
-        ls="",
-        marker="o",
-        alpha=0.5,
-        zorder=20,
-    ),
+    *sotonmet.get_train_test_plot_lines(),
     plot_name="Tide height (m) vs time (days)",
 )
 
-y_pred_mean = data.parse_column(
-    data_dict,
+y_pred_mean = sotonmet.get_column_data(
     "Independent tide height prediction (m)",
 )
-y_pred_std = data.parse_column(
-    data_dict,
+y_pred_std = sotonmet.get_column_data(
     "Independent tide height deviation (m)",
 )
 
 plotting.plot(
-    plotting.Line(
-        t_data,
-        y_data,
-        c="k",
-        ls="",
-        marker="o",
-        alpha=0.5,
-        zorder=20,
-    ),
-    plotting.Line(
-        t_pred,
-        data.parse_column(data_dict, "True tide height (m)"),
-        c="k",
-        ls="",
-        marker="x",
-        alpha=0.5,
-        zorder=20,
-    ),
-    plotting.Line(t_pred, y_pred_mean, c="r", zorder=40),
+    *sotonmet.get_train_test_plot_lines(),
+    plotting.Line(sotonmet.t_truth, y_pred_mean, c="r", zorder=40),
     plotting.FillBetween(
-        t_pred,
+        sotonmet.t_truth,
         y_pred_mean + 2*y_pred_std,
         y_pred_mean - 2*y_pred_std,
         color="r",
