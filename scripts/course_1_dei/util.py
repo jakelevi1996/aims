c5667617076dcde698b4a449f76d85cc2fb50f2a
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-17 22:15:30 +0100

    Update scripts.course_1_dei.util module to use new directory structure

--- scripts/course_1_dei/util.py
+++ scripts/course_1_dei/util.py
@@ -1,16 +1,14 @@
 import gp
-import mean
-import kernel
 
 def get_optimal_gp():
     g = gp.GaussianProcess(
-        prior_mean_func=mean.Constant(offset=3),
-        kernel_func=kernel.Sum(
-            kernel.SquaredExponential(
+        prior_mean_func=gp.mean.Constant(offset=3),
+        kernel_func=gp.kernel.Sum(
+            gp.kernel.SquaredExponential(
                 length_scale=0.06917512071945595,
                 kernel_scale=0.029895345214372513,
             ),
-            kernel.Periodic(
+            gp.kernel.Periodic(
                 period=0.514954586260453,
                 length_scale=0.6512752017924203,
                 kernel_scale=0.13082827454160073,
