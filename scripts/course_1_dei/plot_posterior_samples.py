310c635a48a6c43008ad9e97404eb9a23199e5d5
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-18 09:19:53 +0100

    Update plot_posterior_samples.py to use new interfaces

--- scripts/course_1_dei/plot_posterior_samples.py
+++ scripts/course_1_dei/plot_posterior_samples.py
@@ -2,41 +2,43 @@ import numpy as np
 import __init__
 import data
 import gp
-import mean
-import kernel
 import plotting
+import scripts.course_1_dei.gp_utils
 
 sotonmet = data.Sotonmet()
-t_pred = np.linspace(-1, 6, 1000)
 
 g = gp.GaussianProcess(
-    prior_mean_func=mean.Constant(3),
-    kernel_func=kernel.SquaredExponential(0.08666701, 0.65337298),
+    prior_mean_func=gp.mean.Constant(3),
+    kernel_func=gp.kernel.SquaredExponential(0.08666701, 0.65337298),
     noise_std=0.02931095,
 )
 g.condition(sotonmet.t_train, sotonmet.y_train)
-y_pred_mean, y_pred_std = g.predict(t_pred)
+y_pred_mean, y_pred_std = g.predict(sotonmet.t_pred)
 
 num_posterior_samples = 5
-posterior_samples = g.sample_posterior(t_pred, num_posterior_samples)
+posterior_samples = g.sample_posterior(sotonmet.t_pred, num_posterior_samples)
 
 posterior_sample_lines = [
-    plotting.Line(t_pred, posterior_samples[:, i], c="r", alpha=0.2, zorder=30)
+    plotting.Line(
+        sotonmet.t_pred,
+        posterior_samples[:, i],
+        c="r",
+        alpha=0.2,
+        zorder=30,
+    )
     for i in range(num_posterior_samples)
 ]
 
 plotting.plot(
-    *sotonmet.get_train_test_plot_lines(),
-    plotting.Line(t_pred, y_pred_mean, c="r", zorder=40),
-    plotting.FillBetween(
-        t_pred,
-        y_pred_mean + 2 * y_pred_std,
-        y_pred_mean - 2 * y_pred_std,
-        color="r",
-        lw=0,
-        alpha=0.2,
-        zorder=30,
+    *scripts.course_1_dei.gp_utils.get_dataset_lines(sotonmet),
+    *scripts.course_1_dei.gp_utils.get_gp_prediction_lines(
+        sotonmet.t_pred,
+        y_pred_mean,
+        y_pred_std,
     ),
     *posterior_sample_lines,
     plot_name="Data and GP predictions and posterior samples",
+    dir_name=scripts.course_1_dei.gp_utils.RESULTS_DIR,
+    axis_properties=scripts.course_1_dei.gp_utils.AXIS_PROPERTIES,
+    legend_properties=plotting.LegendProperties(),
 )
