89d235fc9ce7d52e0bb6d0c45d9951b532f6dd31
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-18 17:59:39 +0100

    Plot samples from prior and predictions for 2 different Gaussian processes

--- scripts/course_1_dei/plot_gp_predictions.py
+++ scripts/course_1_dei/plot_gp_predictions.py
@@ -7,48 +7,57 @@ import scripts.course_1_dei.gp_utils
 
 sotonmet = data.Sotonmet()
 
-g = gp.GaussianProcess(
-    prior_mean_func=gp.mean.Constant(3),
-    kernel_func=gp.kernel.SquaredExponential(0.3, 10),
-    noise_std=1,
-)
-g.condition(sotonmet.t_train, sotonmet.y_train)
-y_pred_mean, y_pred_std = g.predict(sotonmet.t_pred)
-
-plotting.plot(
-    *scripts.course_1_dei.gp_utils.get_dataset_lines(sotonmet),
-    *scripts.course_1_dei.gp_utils.get_gp_prediction_lines(
-        sotonmet.t_pred,
-        y_pred_mean,
-        y_pred_std,
+gp_list = [
+    gp.GaussianProcess(
+        prior_mean_func=gp.mean.Constant(3),
+        kernel_func=gp.kernel.SquaredExponential(0.1, 1),
+        noise_std=0.001,
     ),
-    plot_name="Data and GP predictions",
-    dir_name=scripts.course_1_dei.gp_utils.RESULTS_DIR,
-    axis_properties=scripts.course_1_dei.gp_utils.AXIS_PROPERTIES,
-    legend_properties=plotting.LegendProperties(),
-)
+    gp.GaussianProcess(
+        prior_mean_func=gp.mean.Constant(3),
+        kernel_func=gp.kernel.SquaredExponential(0.3, 10),
+        noise_std=1,
+    )
+]
 
-print("Log marginal likelihood = %f" % g.log_marginal_likelihood())
-print("RMSE (train) = %f" % g.rmse(sotonmet.t_train, sotonmet.y_train))
-print("RMSE (truth) = %f" % g.rmse(sotonmet.t_truth, sotonmet.y_truth))
-print(
-    "Log predictive likelihood = %f"
-    % g.log_predictive_likelihood(sotonmet.t_truth, sotonmet.y_truth)
-)
-print(
-    "Log predictive likelihood (train)= %f"
-    % g.log_predictive_likelihood(sotonmet.t_train, sotonmet.y_train)
-)
-for _ in range(5):
-    batch_inds = np.random.choice(
-        sotonmet.n_truth,
-        sotonmet.n_train,
-        replace=False,
+for g in gp_list:
+    g.condition(sotonmet.t_train, sotonmet.y_train)
+    y_pred_mean, y_pred_std = g.predict(sotonmet.t_pred)
+
+    plotting.plot(
+        *scripts.course_1_dei.gp_utils.get_dataset_lines(sotonmet),
+        *scripts.course_1_dei.gp_utils.get_gp_prediction_lines(
+            sotonmet.t_pred,
+            y_pred_mean,
+            y_pred_std,
+        ),
+        plot_name="Data and GP predictions, GP = %r" % g,
+        dir_name=scripts.course_1_dei.gp_utils.RESULTS_DIR,
+        axis_properties=scripts.course_1_dei.gp_utils.AXIS_PROPERTIES,
+        legend_properties=plotting.LegendProperties(),
+    )
+
+    print("Log marginal likelihood = %f" % g.log_marginal_likelihood())
+    print("RMSE (train) = %f" % g.rmse(sotonmet.t_train, sotonmet.y_train))
+    print("RMSE (truth) = %f" % g.rmse(sotonmet.t_truth, sotonmet.y_truth))
+    print(
+        "Log predictive likelihood = %f"
+        % g.log_predictive_likelihood(sotonmet.t_truth, sotonmet.y_truth)
     )
     print(
-        "Log predictive likelihood (truth subset)= %f"
-        % g.log_predictive_likelihood(
-            sotonmet.t_truth[batch_inds],
-            sotonmet.y_truth[batch_inds],
-        )
+        "Log predictive likelihood (train)= %f"
+        % g.log_predictive_likelihood(sotonmet.t_train, sotonmet.y_train)
     )
+    for _ in range(5):
+        batch_inds = np.random.choice(
+            sotonmet.n_truth,
+            sotonmet.n_train,
+            replace=False,
+        )
+        print(
+            "Log predictive likelihood (truth subset)= %f"
+            % g.log_predictive_likelihood(
+                sotonmet.t_truth[batch_inds],
+                sotonmet.y_truth[batch_inds],
+            )
+        )
--- scripts/course_1_dei/plot_prior_samples.py
+++ scripts/course_1_dei/plot_prior_samples.py
@@ -7,31 +7,40 @@ import scripts.course_1_dei.gp_utils
 
 sotonmet = data.Sotonmet()
 
-g = gp.GaussianProcess(
-    prior_mean_func=gp.mean.Constant(3),
-    kernel_func=gp.kernel.SquaredExponential(length_scale=0.1, kernel_scale=1),
-    noise_std=0.001,
-)
-num_prior_samples = 5
-prior_samples = g.sample_prior(sotonmet.t_pred, num_prior_samples)
-
-cp = plotting.ColourPicker(num_prior_samples)
-prior_sample_lines = [
-    plotting.Line(
-        sotonmet.t_pred,
-        prior_samples[:, i],
-        c=cp(i),
-        alpha=0.5,
-        zorder=30,
-        label="Prior sample %i" % (i + 1)
+gp_list = [
+    gp.GaussianProcess(
+        prior_mean_func=gp.mean.Constant(3),
+        kernel_func=gp.kernel.SquaredExponential(0.1, 1),
+        noise_std=0.001,
+    ),
+    gp.GaussianProcess(
+        prior_mean_func=gp.mean.Constant(3),
+        kernel_func=gp.kernel.SquaredExponential(0.3, 10),
+        noise_std=1,
     )
-    for i in range(num_prior_samples)
 ]
-plotting.plot(
-    *scripts.course_1_dei.gp_utils.get_dataset_lines(sotonmet),
-    *prior_sample_lines,
-    plot_name="Samples from GP prior",
-    dir_name=scripts.course_1_dei.gp_utils.RESULTS_DIR,
-    axis_properties=scripts.course_1_dei.gp_utils.AXIS_PROPERTIES,
-    legend_properties=plotting.LegendProperties(),
-)
+
+for g in gp_list:
+    num_prior_samples = 5
+    prior_samples = g.sample_prior(sotonmet.t_pred, num_prior_samples)
+
+    cp = plotting.ColourPicker(num_prior_samples)
+    prior_sample_lines = [
+        plotting.Line(
+            sotonmet.t_pred,
+            prior_samples[:, i],
+            c=cp(i),
+            alpha=0.5,
+            zorder=30,
+            label="Prior sample %i" % (i + 1)
+        )
+        for i in range(num_prior_samples)
+    ]
+    plotting.plot(
+        *scripts.course_1_dei.gp_utils.get_dataset_lines(sotonmet),
+        *prior_sample_lines,
+        plot_name="Samples from GP prior, GP = %r" % g,
+        dir_name=scripts.course_1_dei.gp_utils.RESULTS_DIR,
+        axis_properties=scripts.course_1_dei.gp_utils.AXIS_PROPERTIES,
+        legend_properties=plotting.LegendProperties(),
+    )
