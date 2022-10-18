1a4c14097df372514f754db1f5d24d2f8bd65b22
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-18 21:00:40 +0100

    Plot samples from predictive distribution of 2 different GPs

--- scripts/course_1_dei/plot_posterior_samples.py
+++ scripts/course_1_dei/plot_posterior_samples.py
@@ -7,11 +7,19 @@ import scripts.course_1_dei.gp_utils
 
 sotonmet = data.Sotonmet()
 
-g = gp.GaussianProcess(
-    prior_mean_func=gp.mean.Constant(3),
-    kernel_func=gp.kernel.SquaredExponential(0.3, 10),
-    noise_std=1,
-)
+gp_list = [
+    gp.GaussianProcess(
+        prior_mean_func=gp.mean.Constant(3),
+        kernel_func=gp.kernel.SquaredExponential(0.1, 1),
+        noise_std=0.001,
+    ),
+    gp.GaussianProcess(
+        prior_mean_func=gp.mean.Constant(3),
+        kernel_func=gp.kernel.SquaredExponential(0.3, 10),
+        noise_std=1,
+    ),
+]
 
-plot_name = "Data and GP predictions and posterior samples"
-scripts.course_1_dei.gp_utils.plot_gp(g, sotonmet, plot_name)
+for g in gp_list:
+    plot_name = "Data and GP predictions and posterior samples, GP = %r" % g
+    scripts.course_1_dei.gp_utils.plot_gp(g, sotonmet, plot_name)
