b6fa8dc9ffdfdbad3b14d0c7c1dd4f5f5b8d3173
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-18 17:50:41 +0100

    Update plot_prior_samples.py to use new interface

--- scripts/course_1_dei/plot_prior_samples.py
+++ scripts/course_1_dei/plot_prior_samples.py
@@ -2,41 +2,32 @@ import numpy as np
 import __init__
 import data
 import gp
-import mean
-import kernel
 import plotting
+import scripts.course_1_dei.gp_utils
 
-data_dict, num_columns, num_rows = data.load_dict()
-assert num_columns == 19
-assert num_rows == 1258
-t_pred, t_data, y_data = data.parse_dict(data_dict)
-assert t_pred.size == 1258
-assert t_data.size == 917
-assert y_data.size == 917
+sotonmet = data.Sotonmet()
 
 g = gp.GaussianProcess(
-    prior_mean_func=mean.Constant(3),
-    kernel_func=kernel.SquaredExponential(length_scale=0.1, kernel_scale=1),
+    prior_mean_func=gp.mean.Constant(3),
+    kernel_func=gp.kernel.SquaredExponential(length_scale=0.1, kernel_scale=1),
     noise_std=0.001,
 )
 num_prior_samples = 5
-prior_samples = g.sample_prior(t_pred, num_prior_samples)
+prior_samples = g.sample_prior(sotonmet.t_pred, num_prior_samples)
 
 cp = plotting.ColourPicker(num_prior_samples)
 prior_sample_lines = [
-    plotting.Line(t_pred, prior_samples[:, i], c=cp(i), alpha=0.5, zorder=30)
+    plotting.Line(
+        sotonmet.t_pred,
+        prior_samples[:, i],
+        c=cp(i),
+        alpha=0.5,
+        zorder=30,
+    )
     for i in range(num_prior_samples)
 ]
 plotting.plot(
-    plotting.Line(
-        t_data,
-        y_data,
-        c="k",
-        ls="",
-        marker="o",
-        alpha=0.5,
-        zorder=20,
-    ),
+    *scripts.course_1_dei.gp_utils.get_dataset_lines(sotonmet),
     *prior_sample_lines,
     plot_name="Samples from GP prior vs data",
 )
