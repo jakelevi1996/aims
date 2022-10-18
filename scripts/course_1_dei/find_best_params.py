d2ba53ad6e2cb83a5719a5adcb17c0f31dcc599d
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-18 23:18:40 +0100

    Make print statement in script more descriptive

--- scripts/course_1_dei/find_best_params.py
+++ scripts/course_1_dei/find_best_params.py
@@ -19,7 +19,10 @@ g.optimise_hyperparameters(sotonmet.t_train, sotonmet.y_train)
 g.decondition()
 g.condition(sotonmet.t_train, sotonmet.y_train)
 y_pred_mean, y_pred_std = g.predict(sotonmet.t_pred)
-print(g.log_marginal_likelihood())
+print(
+    "Final GP = %r\nFinal log likelihood = %f"
+    % (g, g.log_marginal_likelihood())
+)
 
 scripts.course_1_dei.gp_utils.plot_gp(
     g,
