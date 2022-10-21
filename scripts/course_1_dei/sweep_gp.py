ade66b7728e58651350fb60215b96e6304a76893
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-21 02:51:08 +0100

    Update optimal hyperparameters to match new implementation of Periodic kernel

--- scripts/course_1_dei/sweep_gp.py
+++ scripts/course_1_dei/sweep_gp.py
@@ -169,10 +169,10 @@ class Product(_GpSweep):
     def _add_parameters(self):
         self._add_param("offset", 3, np.arange(0, 4, 0.5))
         self._add_log_range_param("sqe_length_scale", 0.7880754080416588)
-        self._add_log_range_param("sqe_kernel_scale", 1.4457480054362188)
+        self._add_log_range_param("sqe_kernel_scale", 5.538930746200925)
         self._add_log_range_param("period", 0.5082224844864489, scale_factor=4)
-        self._add_log_range_param("per_length_scale", 0.7405355857351571)
-        self._add_log_range_param("per_kernel_scale", 0.09929886031253296)
+        self._add_log_range_param("per_length_scale", 0.7270981370167336)
+        self._add_log_range_param("per_kernel_scale", 0.0974970247641131)
         self._add_log_range_param("noise_std", 0.02905329635947378)
 
     def _get_gp(
