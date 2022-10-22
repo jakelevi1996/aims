e8eede42ecb19ef562937407d43419f1eb0e5d73
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-22 19:13:17 +0100

    Add section "Periodic kernels"

--- scripts/course_1_dei/print_latex_tables.py
+++ scripts/course_1_dei/print_latex_tables.py
@@ -4,60 +4,89 @@ import data
 import scripts.course_1_dei.gp_utils
 
 sotonmet = data.Sotonmet()
-gp_name_list = ["sqe_1", "sqe_2", "sqe_opt"]
-gp_list = [
-    scripts.course_1_dei.gp_utils.gp_dict[gp_name]
-    for gp_name in gp_name_list
-]
-for g in gp_list:
+
+print("Conditioning all models...")
+for g in scripts.course_1_dei.gp_utils.gp_dict.values():
     g.condition(sotonmet.t_train, sotonmet.y_train)
 
+# Hyperparameters
 print("\nTable: SQE GPs\n")
+sqe_name_list = ["sqe_1", "sqe_2", "sqe_opt"]
+sqe_list = [
+    scripts.course_1_dei.gp_utils.gp_dict[name]
+    for name in sqe_name_list
+]
 table = [
     ["Hyperparameter"] + [
-        gp_name.replace("_", "\\_") for gp_name in gp_name_list
+        name.replace("_", "\\_") for name in sqe_name_list
     ],
-    ["$c$"] + ["%.4f" % g._prior_mean_func._offset for g in gp_list],
-    ["$\\lambda$"] + ["%.4f" % g._kernel_func._length_scale for g in gp_list],
-    ["$k$"] + ["%.4f" % g._kernel_func._kernel_scale for g in gp_list],
-    ["$\\sigma$"] + ["%.4f" % np.sqrt(g._noise_var) for g in gp_list],
+    ["$c$"]        + ["%.4f" % g._prior_mean_func._offset   for g in sqe_list],
+    ["$\\lambda$"] + ["%.4f" % g._kernel_func._length_scale for g in sqe_list],
+    ["$k$"]        + ["%.4f" % g._kernel_func._kernel_scale for g in sqe_list],
+    ["$\\sigma$"]  + ["%.4f" % np.sqrt(g._noise_var)        for g in sqe_list],
 ]
-print(table)
+
 for row in table:
     print(" & ".join(row), end=" \\\\\n")
 
-print("\nTable: SQE metrics\n")
-table = [
-    ["Metric"] + [
-        gp_name.replace("_", "\\_") for gp_name in gp_name_list
-    ],
-    ["RMSE (train)"] + [
-        "%.4f" % g.rmse(sotonmet.t_train, sotonmet.y_train)
-        for g in gp_list
-    ],
-    ["RMSE (truth)"] + [
-        "%.4f" % g.rmse(sotonmet.t_truth, sotonmet.y_truth)
-        for g in gp_list
-    ],
-    ["LML"] + [
-        "%.1f" % g.log_marginal_likelihood()
-        for g in gp_list
-    ],
-    ["LPL (train)"] + [
-        "%.1f" % g.log_predictive_likelihood(
-            sotonmet.t_train,
-            sotonmet.y_train,
-        )
-        for g in gp_list
-    ],
-    ["LPL (truth)"] + [
-        "%.1f" % g.log_predictive_likelihood(
-            sotonmet.t_truth,
-            sotonmet.y_truth,
-        )
-        for g in gp_list
-    ],
+print("\nTable: PER GPs\n")
+per_name_list = ["per_1", "per_opt"]
+per_list = [
+    scripts.course_1_dei.gp_utils.gp_dict[name]
+    for name in per_name_list
 ]
-print(table)
+table = [
+    ["Hyperparameter"] + [
+        name.replace("_", "\\_") for name in per_name_list
+    ],
+    ["$c$"]        + ["%.4f" % g._prior_mean_func._offset   for g in per_list],
+    ["$\\lambda$"] + ["%.4f" % g._kernel_func._length_scale for g in per_list],
+    ["$k$"]        + ["%.4f" % g._kernel_func._kernel_scale for g in per_list],
+    ["$\\sigma$"]  + ["%.4f" % np.sqrt(g._noise_var)        for g in per_list],
+    ["$T$"]        + ["%.4f" % (np.pi / g._kernel_func._angular_freq) for g in per_list],
+]
+
 for row in table:
     print(" & ".join(row), end=" \\\\\n")
+
+# Metrics
+for gp_name_list in [sqe_name_list, per_name_list]:
+    print("\nTable: %s metrics\n" % gp_name_list)
+    gp_list = [
+        scripts.course_1_dei.gp_utils.gp_dict[name]
+        for name in gp_name_list
+    ]
+    table = [
+        ["Metric"] + [
+            gp_name.replace("_", "\\_") for gp_name in gp_name_list
+        ],
+        ["RMSE (train)"] + [
+            "%.4f" % g.rmse(sotonmet.t_train, sotonmet.y_train)
+            for g in gp_list
+        ],
+        ["RMSE (truth)"] + [
+            "%.4f" % g.rmse(sotonmet.t_truth, sotonmet.y_truth)
+            for g in gp_list
+        ],
+        ["LML"] + [
+            "%.1f" % g.log_marginal_likelihood()
+            for g in gp_list
+        ],
+        ["LPL (train)"] + [
+            "%.1f" % g.log_predictive_likelihood(
+                sotonmet.t_train,
+                sotonmet.y_train,
+            )
+            for g in gp_list
+        ],
+        ["LPL (truth)"] + [
+            "%.1f" % g.log_predictive_likelihood(
+                sotonmet.t_truth,
+                sotonmet.y_truth,
+            )
+            for g in gp_list
+        ],
+    ]
+
+    for row in table:
+        print(" & ".join(row), end=" \\\\\n")
