8ebe89b98769cf1ab8098d21802ab5bb32d6204d
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-25 15:46:00 +0100

    Replace coord_to_ind_dict with x_from_coord

--- scripts/course_3_optimisation/sudoku.py
+++ scripts/course_3_optimisation/sudoku.py
@@ -2,10 +2,8 @@ import cvxpy as cp
 import numpy as np
 
 # Define x (the decision variable), the list of coordinates, and the dictionary
-# mapping coordinates to elements in x. x[coord_to_ind_dict[val, row, col]] ==
-# 1 implies that the element at position (row, col) has value val. In
-# subsequent comments, the shorthand x[[val, row, col]] is used for
-# x[coord_to_ind_dict[val, row, col]]
+# mapping coordinates to elements in x. x_from_coord[val, row, col] ==
+# 1 implies that the element at position (row, col) has value val
 x = cp.Variable(shape=(9*9*9), boolean=True)
 numbers_1_to_9 = list(range(1, 10))
 
@@ -16,59 +14,59 @@ coord_list = [
     for col in numbers_1_to_9
 ]
 
-coord_to_ind_dict = {coord: i for i, coord in enumerate(coord_list)}
-# x_from_coord = {coord: x[i] for i, coord in enumerate(coord_list)}
+x_from_coord = {coord: x[i] for i, coord in enumerate(coord_list)}
 
 # Initialise list of constraints
 constraints = []
 
 # Each element can only have one value, EG in the 3rd row and the 5th column,
-# x[[1, 3, 5]] + x[[2, 3, 5] + ... + x[[9, 3, 5]]] == 1
+# x_from_coord[1, 3, 5] + x_from_coord[2, 3, 5] + ... + x_from_coord[9, 3, 5]
+# == 1
 for row in numbers_1_to_9:
     for col in numbers_1_to_9:
         constraints.append(
             sum(
-                x[coord_to_ind_dict[val, row, col]]
+                x_from_coord[val, row, col]
                 for val in numbers_1_to_9
             ) == 1
         )
 
 # Each value can only be in each row once, EG for the value 2 in the 8th row,
-# x[[2, 8, 1]] + x[[2, 8, 2]] + ... + x[[2, 8, 9]] == 1
+# x_from_coord[2, 8, 1] + x_from_coord[2, 8, 2] + ... + x_from_coord[2, 8, 9]
+# == 1
 for val in numbers_1_to_9:
     for row in numbers_1_to_9:
         constraints.append(
             sum(
-                x[coord_to_ind_dict[val, row, col]]
+                x_from_coord[val, row, col]
                 for col in numbers_1_to_9
             ) == 1
         )
 
 # Each value can only be in each column once, EG for the value 7 in the 4th
-# column, x[[7, 1, 4]] + x[[7, 2, 4]] + ... + x[[7, 9, 4]] == 1
+# column, x_from_coord[7, 1, 4] + x_from_coord[7, 2, 4] + ... + x_from_coord[7,
+# 9, 4] == 1
 for val in numbers_1_to_9:
     for col in numbers_1_to_9:
         constraints.append(
             sum(
-                x[coord_to_ind_dict[val, row, col]]
+                x_from_coord[val, row, col]
                 for row in numbers_1_to_9
             ) == 1
         )
 
 # Each value can only be in each 3x3 cell once, EG for the value 8 in the cell
-# which is 3 down and 2 accross, x[[8, 7, 4]] + x[[8, 7, 5]] + ... + x[[8, 9,
-# 6]] == 1
+# which is 3 down and 2 accross, x_from_coord[8, 7, 4] + x_from_coord[8, 7, 5]
+#+ ... + x_from_coord[8, 9, 6]] == 1
 for val in numbers_1_to_9:
     for cell_row in [0, 1, 2]:
         for cell_col in [0, 1, 2]:
             constraints.append(
                 sum(
-                    x[
-                        coord_to_ind_dict[
-                            val,
-                            3 * cell_row + row_in_cell,
-                            3 * cell_col + col_in_cell,
-                        ]
+                    x_from_coord[
+                        val,
+                        3 * cell_row + row_in_cell,
+                        3 * cell_col + col_in_cell,
                     ]
                     for row_in_cell in [1, 2, 3]
                     for col_in_cell in [1, 2, 3]
@@ -90,12 +88,11 @@ known_values = [
 for row, row_list in zip(numbers_1_to_9, known_values):
     for col, val in zip(numbers_1_to_9, row_list):
         if val > 0:
-            constraints.append(x[coord_to_ind_dict[val, row, col]] == 1)
+            constraints.append(x_from_coord[val, row, col] == 1)
 
 objective = cp.Maximize(0)
 prob = cp.Problem(objective, constraints)
-print(prob.solve())
-print(x.value)
+prob.solve()
 
 # Convert solution into a numpy array and print
 s = np.zeros(shape=[9, 9])
