37ec745fa464776a108ae89fa09ebdc6f50dd8b9
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-25 20:36:29 +0100

    Add svm module with solve_separable and solve functions

---
+++ scripts/course_3_optimisation/svm.py
@@ -0,0 +1,20 @@
+import cvxpy as cp
+
+def solve_separable(x, labels, x_dim, n):
+    a = cp.Variable(shape=x_dim)
+    b = cp.Variable(shape=1)
+    constraints = [cp.multiply(labels, x @ a + b) >= 1]
+    objective = cp.Minimize(cp.sum_squares(a))
+    prob = cp.Problem(objective, constraints)
+    prob.solve()
+    return a.value, b.value
+
+def solve(x, labels, x_dim, n, norm_penalty=0):
+    t = cp.Variable(shape=n)
+    a = cp.Variable(shape=x_dim)
+    b = cp.Variable(shape=1)
+    constraints = [t >= 0, t >= (1 - cp.multiply(labels, x @ a + b))]
+    objective = cp.Minimize(cp.sum(t) + norm_penalty * cp.sum_squares(a))
+    prob = cp.Problem(objective, constraints)
+    prob.solve()
+    return a.value, b.value
