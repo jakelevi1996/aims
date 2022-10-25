e15b8cde7402c629c71e205ed85ddec63ed006c6
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-25 15:54:00 +0100

    Wrap comments

--- scripts/course_3_optimisation/sudoku.py
+++ scripts/course_3_optimisation/sudoku.py
@@ -9,9 +9,9 @@ def solve(known_values):
     value in that row. The elements in the inner list should be a number from 1
     to 9 if that number is known, or 0 if that value is unknown.
     """
-    # Define x (the decision variable), the list of coordinates, and the dictionary
-    # mapping coordinates to elements in x. x_from_coord[val, row, col] ==
-    # 1 implies that the element at position (row, col) has value val
+    # Define x (the decision variable), the list of coordinates, and the
+    # dictionary mapping coordinates to elements in x. x_from_coord[val, row,
+    # col] == 1 implies that the element at position (row, col) has value val
     x = cp.Variable(shape=(9*9*9), boolean=True)
     numbers_1_to_9 = list(range(1, 10))
 
@@ -27,9 +27,9 @@ def solve(known_values):
     # Initialise list of constraints
     constraints = []
 
-    # Each element can only have one value, EG in the 3rd row and the 5th column,
-    # x_from_coord[1, 3, 5] + x_from_coord[2, 3, 5] + ... + x_from_coord[9, 3, 5]
-    # == 1
+    # Each element can only have one value, EG in the 3rd row and the 5th
+    # column, x_from_coord[1, 3, 5] + x_from_coord[2, 3, 5] + ... +
+    # x_from_coord[9, 3, 5] == 1
     for row in numbers_1_to_9:
         for col in numbers_1_to_9:
             constraints.append(
@@ -39,9 +39,9 @@ def solve(known_values):
                 ) == 1
             )
 
-    # Each value can only be in each row once, EG for the value 2 in the 8th row,
-    # x_from_coord[2, 8, 1] + x_from_coord[2, 8, 2] + ... + x_from_coord[2, 8, 9]
-    # == 1
+    # Each value can only be in each row once, EG for the value 2 in the 8th
+    # row, x_from_coord[2, 8, 1] + x_from_coord[2, 8, 2] + ... +
+    # x_from_coord[2, 8, 9] == 1
     for val in numbers_1_to_9:
         for row in numbers_1_to_9:
             constraints.append(
@@ -52,8 +52,8 @@ def solve(known_values):
             )
 
     # Each value can only be in each column once, EG for the value 7 in the 4th
-    # column, x_from_coord[7, 1, 4] + x_from_coord[7, 2, 4] + ... + x_from_coord[7,
-    # 9, 4] == 1
+    # column, x_from_coord[7, 1, 4] + x_from_coord[7, 2, 4] + ... +
+    # x_from_coord[7, 9, 4] == 1
     for val in numbers_1_to_9:
         for col in numbers_1_to_9:
             constraints.append(
@@ -63,8 +63,9 @@ def solve(known_values):
                 ) == 1
             )
 
-    # Each value can only be in each 3x3 cell once, EG for the value 8 in the cell
-    # which is 3 down and 2 accross, x_from_coord[8, 7, 4] + x_from_coord[8, 7, 5]
+    # Each value can only be in each 3x3 cell once, EG for the value 8 in the
+    # cell which is 3 down and 2 accross, x_from_coord[8, 7, 4] +
+    # x_from_coord[8, 7, 5]
     #+ ... + x_from_coord[8, 9, 6]] == 1
     for val in numbers_1_to_9:
         for cell_row in [0, 1, 2]:
