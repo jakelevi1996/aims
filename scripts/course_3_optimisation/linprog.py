88801910ac12e97a399e766346dc337befa73b7d
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-29 13:36:58 +0100

    Plot norm of difference between projections vs iteration

--- scripts/course_3_optimisation/linprog.py
+++ scripts/course_3_optimisation/linprog.py
@@ -3,6 +3,8 @@ import scipy.linalg
 import cvxpy as cp
 import __init__
 import util
+import plotting
+import scripts.course_3_optimisation
 
 def gen_problem(n=500, m=100, rng=None):
     if rng is None:
@@ -49,12 +51,14 @@ def solve_pg(
     g = np.block([0, b, -c])
     z = np.zeros((2 * n) + m)
     clip_inds = np.block([np.ones(n), np.zeros(m), np.ones(n)])
+    error_norm_list = []
     for _ in range(n_iterations):
         z_hat = z + F.T @ scipy.linalg.lu_solve(F_fact, g - F @ z)
         z = np.where(clip_inds, np.clip(z_hat, 0, None), z_hat)
+        error_norm_list.append(np.linalg.norm(z - z_hat))
 
     x = z[:n]
-    return x
+    return x, error_norm_list
 
 rng = np.random.default_rng(2)
 util.numpy_set_print_options()
@@ -68,7 +72,7 @@ p, x = solve_cp(A, b, c)
 timer.print_time_taken()
 
 timer = util.Timer()
-x_pg = solve_pg(A, b, c)
+x_pg, error_norm_list = solve_pg(A, b, c)
 timer.print_time_taken()
 
 print(np.linalg.norm(x - x_pg))
@@ -77,3 +81,14 @@ print(np.max(x), np.max(x_pg))
 print(np.min(x), np.min(x_pg))
 print(np.linalg.norm(A @ x - b), np.linalg.norm(A @ x_pg - b))
 print(np.linalg.norm(c @ x), np.linalg.norm(c @ x_pg))
+
+plotting.plot(
+    plotting.Line(np.arange(len(error_norm_list)), error_norm_list, c="b"),
+    axis_properties=plotting.AxisProperties(
+        xlabel="Iteration",
+        log_yscale=True,
+        ylim=[0.1, 10],
+    ),
+    plot_name="$\\Vert z - \\tilde{z} \\Vert_2$",
+    dir_name=scripts.course_3_optimisation.RESULTS_DIR,
+)
