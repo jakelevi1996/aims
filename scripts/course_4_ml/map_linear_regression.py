6f23a24905deef41b4b1a5f4d81680cd2163246f
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-31 19:45:03 +0000

    Find and plot predictions of best polynomial order

--- scripts/course_4_ml/map_linear_regression.py
+++ scripts/course_4_ml/map_linear_regression.py
@@ -51,9 +51,73 @@ plotting.plot(
     plotting.Line(Xtest, y_pred_mle, c="r", label="MLE prediction"),
     plotting.Line(Xtest, y_pred_map, c="m", label="MAP prediction"),
     axis_properties=plotting.AxisProperties("$x$", "$y$", ylim=[-5, 5]),
-    plot_name="MAP vs MLE predictions",
+    plot_name="MAP vs MLE predictions, polynomial degree = 8",
     legend_properties=plotting.LegendProperties(),
 )
 
 util.numpy_set_print_options()
 print("MLE:", mle_model, "MAP:", map_model, sep="\n")
+
+# Plot RMSE vs polynomial order
+K_max = 12 # this is the maximum degree of polynomial we will consider
+assert(K_max < N)
+
+rmse_mle = []
+rmse_map = []
+
+def rmse(y, ypred):
+    return np.sqrt(np.mean(np.square(y - ypred)))
+
+for k in range(K_max+1):
+    mle_model = linear_regression.LinearRegression(
+        features=linear_regression.features.Polynomial(degree=k),
+    )
+    map_model = linear_regression.LinearRegression(
+        features=linear_regression.features.Polynomial(degree=k),
+    )
+    mle_model.estimate_ml(X, y)
+    map_model.estimate_map(X, y, sigma, alpha)
+
+    y_pred_mle = mle_model.predict(Xtest)
+    y_pred_map = map_model.predict(Xtest)
+
+    rmse_mle.append(rmse(ytest, y_pred_mle))
+    rmse_map.append(rmse(ytest, y_pred_map))
+
+plotting.plot(
+    plotting.Line(rmse_mle, c="r", label="Maximum likelihood"),
+    plotting.Line(rmse_map, c="m", label="MAP"),
+    axis_properties=plotting.AxisProperties(
+        xlabel="degree of polynomial",
+        ylabel="RMSE",
+        log_yscale=True,
+    ),
+    plot_name="RMSE vs degree polynomial, comparing MLE vs MAP",
+    legend_properties=plotting.LegendProperties(),
+)
+
+# Plot predictions of best polynomial order
+K = 4 # polynomial degree
+
+# feature matrix
+mle_model = linear_regression.LinearRegression(
+    features=linear_regression.features.Polynomial(degree=K),
+)
+mle_model.estimate_ml(X, y)
+map_model = linear_regression.LinearRegression(
+    features=linear_regression.features.Polynomial(degree=K),
+)
+map_model.estimate_map(X, y, sigma, alpha)
+
+y_pred_mle = mle_model.predict(Xtest)
+y_pred_map = map_model.predict(Xtest)
+
+plotting.plot(
+    plotting.Line(X, y, marker="+", c="b", ls="", label="Training data"),
+    plotting.Line(Xtest, g(Xtest, 0), c="g", label="Ground truth function"),
+    plotting.Line(Xtest, y_pred_mle, c="r", label="MLE prediction"),
+    plotting.Line(Xtest, y_pred_map, c="m", label="MAP prediction"),
+    axis_properties=plotting.AxisProperties("$x$", "$y$"),
+    plot_name="MAP vs MLE predictions, polynomial degree = 4",
+    legend_properties=plotting.LegendProperties(),
+)
