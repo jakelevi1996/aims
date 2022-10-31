f7b85b5f1fe7afcd313f52a9a170b72edecf94bc
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-10-31 15:50:28 +0000

    Compare different values of the model parameter

--- scripts/course_4_ml/linear_regression.py
+++ scripts/course_4_ml/linear_regression.py
@@ -29,8 +29,18 @@ X = np.array([-3, -1, 0, 1, 3]).reshape(-1,1) # 5x1 vector, N=5, D=1
 y = np.array([-1.2, -0.7, 0.14, 0.67, 1.67]).reshape(-1,1) # 5x1 vector
 
 # Plot the training set
+data_line = plotting.Line(
+    X,
+    y,
+    marker="+",
+    ms=10,
+    ls="",
+    c="b",
+    label="Data",
+    zorder=30,
+)
 plotting.plot(
-    plotting.Line(X, y, marker="+", markersize=10, ls="", c="b"),
+    data_line,
     axis_properties=plotting.AxisProperties("$x$", "$y$"),
     plot_name="Linear regression dataset",
 )
@@ -45,9 +55,33 @@ model.estimate_ml(X, y)
 ml_prediction = model.predict(Xtest)
 
 # plot
+mle_prediction_line = plotting.Line(
+    Xtest,
+    ml_prediction,
+    c="r",
+    label="MLE estimate",
+    zorder=20,
+)
 plotting.plot(
-    plotting.Line(X, y, marker="+", markersize=10, ls="", c="b"),
-    plotting.Line(Xtest, ml_prediction, c="r"),
+    data_line,
+    mle_prediction_line,
     axis_properties=plotting.AxisProperties("$x$", "$y$"),
     plot_name="Linear regression prediction",
+)
+
+# Compare different values of the model parameter
+line_list = [data_line, mle_prediction_line]
+param_list = np.arange(1, 11, 1) / 10
+cp = plotting.ColourPicker(param_list.size, cyclic=False)
+for i, param in enumerate(param_list):
+    model._params = np.array(param).reshape(model._params.shape)
+    Ytest = model.predict(Xtest)
+    line_list.append(
+        plotting.Line(Xtest, Ytest, c=cp(i), label="Theta = %.1f" % param)
+    )
+plotting.plot(
+    *line_list,
+    axis_properties=plotting.AxisProperties("$x$", "$y$"),
+    plot_name="Comparing different values for $\\theta$",
+    legend_properties=plotting.LegendProperties(),
 )
