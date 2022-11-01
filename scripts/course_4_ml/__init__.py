508de1c048062053e00a52de3ce86cb08e6e349c
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-11-01 14:24:07 +0000

    Add outline for autodifff module and unit test

--- scripts/course_4_ml/__init__.py
+++ scripts/course_4_ml/__init__.py
@@ -4,6 +4,7 @@ import sys
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "..", ".."))
 
+sys.path.append(CURRENT_DIR)
 sys.path.append(ROOT_DIR)
 
 RESULTS_DIR = os.path.join(CURRENT_DIR, "Results")
