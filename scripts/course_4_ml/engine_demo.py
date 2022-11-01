ff08fd3241382437ab0889eb2a35b348c702061c
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-11-01 19:46:27 +0000

    Test gradient where a node appears twice

--- scripts/course_4_ml/engine_demo.py
+++ scripts/course_4_ml/engine_demo.py
@@ -1,7 +1,7 @@
 import engine
 
 a = engine.Value(3)
-b = engine.Value(4)
+b = engine.Value(2) + engine.Value(2)
 c = a + b
 print(a, b, c)
 c.backward()
@@ -11,3 +11,10 @@ e = c + d
 print(e.get_all_children())
 print(e.backward())
 print(e.grad, d.grad, c.grad, a.grad, b.grad)
+f = b + e
+print(f.get_all_children())
+print(c.get_all_children())
+print(e.get_all_children())
+print(e.get_all_children())
+print(f.backward())
+print(f.grad, e.grad, d.grad, c.grad, a.grad, b.grad)
