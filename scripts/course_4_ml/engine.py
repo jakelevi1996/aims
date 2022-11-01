ad95a27504a055102d0497f6ed2a424fb27c8aa8
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-11-01 15:33:48 +0000

    Fill in methods (but not backward or _backward methods)

--- scripts/course_4_ml/engine.py
+++ scripts/course_4_ml/engine.py
@@ -3,17 +3,26 @@ import math
 class Value:
     """ stores a single scalar value and its gradient """
 
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _op=''):
         self.data = data
         self.grad = 0
         # internal variables used for autograd graph construction
         self._backward = lambda: None # this will be called on the backward pass
-        self._prev = set(_children)
+        self._children = set()
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
 
+    def add_child(self, other):
+        self._children.add(other)
+
+    def _to_value(self, other):
+        return other if isinstance(other, Value) else Value(other)
+
     def __add__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data, (self, other), '+')
+        other = self._to_value(other)
+        out_data = self.data + other.data
+        out = Value(out_data, '+')
+        out.add_child(self)
+        out.add_child(other)
 
         def _backward():
             self.grad += out.grad
@@ -23,22 +32,74 @@ class Value:
         return out
 
     def __mul__(self, other):
-        raise NotImplementedError
+        other = self._to_value(other)
+        out_data = self.data * other.data
+        out = Value(out_data, '*')
+        out.add_child(self)
+        out.add_child(other)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def __pow__(self, other):
-        raise NotImplementedError
+        other = self._to_value(other)
+        out_data = self.data ** other.data
+        out = Value(out_data, '^')
+        out.add_child(self)
+        out.add_child(other)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def relu(self):
-        raise NotImplementedError
+        out_data = max(self.data, 0)
+        out = Value(out_data, 'relu')
+        out.add_child(self)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def sigmoid(self):
-        raise NotImplementedError
+        out_data = 1.0 / (1.0 + math.exp(self.data))
+        out = Value(out_data, 'sigmoid')
+        out.add_child(self)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def cos(self):
-        raise NotImplementedError
+        out_data = math.cos(self.data)
+        out = Value(out_data, 'cos')
+        out.add_child(self)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def sin(self):
-        raise NotImplementedError
+        out_data = math.sin(self.data)
+        out = Value(out_data, 'sin')
+        out.add_child(self)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def backward(self):
 
@@ -49,6 +110,8 @@ class Value:
             # this needs to fill topo with the nodes in a topological ordering of the graph
             # so we can visit them and call their _backward() in the correct order
 
+            # for p in self._prev:
+
             raise NotImplementedError
         build_topo(self)
 
@@ -58,25 +121,93 @@ class Value:
             v._backward()
 
     def __neg__(self): # -self
-        raise NotImplementedError
+        out_data = -self.data
+        out = Value(out_data, 'sin')
+        out.add_child(self)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def __radd__(self, other): # other + self
-        raise NotImplementedError
+        other = self._to_value(other)
+        out_data = self.data + other.data
+        out = Value(out_data, '+')
+        out.add_child(self)
+        out.add_child(other)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def __sub__(self, other): # self - other
-        raise NotImplementedError
+        other = self._to_value(other)
+        out_data = self.data - other.data
+        out = Value(out_data, '-')
+        out.add_child(self)
+        out.add_child(other)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def __rsub__(self, other): # other - self
-        raise NotImplementedError
+        other = self._to_value(other)
+        out_data = other.data - self.data
+        out = Value(out_data, '-')
+        out.add_child(self)
+        out.add_child(other)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def __rmul__(self, other): # other * self
-        raise NotImplementedError
+        other = self._to_value(other)
+        out_data = other.data * self.data
+        out = Value(out_data, '*')
+        out.add_child(self)
+        out.add_child(other)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def __truediv__(self, other): # self / other
-        raise NotImplementedError
+        other = self._to_value(other)
+        out_data = self.data / other.data
+        out = Value(out_data, '/')
+        out.add_child(self)
+        out.add_child(other)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def __rtruediv__(self, other): # other / self
-        raise NotImplementedError
+        other = self._to_value(other)
+        out_data = other.data / self.data
+        out = Value(out_data, '/')
+        out.add_child(self)
+        out.add_child(other)
+
+        def _backward():
+            raise NotImplementedError
+        out._backward = _backward
+
+        return out
 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
