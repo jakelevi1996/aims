5e40bff02497d25c30a108e9ac377df3336b6e6d
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-11-01 16:33:20 +0000

    Print output of get_all_children method and correct typos

--- scripts/course_4_ml/engine_demo.py
+++ scripts/course_4_ml/engine_demo.py
@@ -4,7 +4,10 @@ a = engine.Value(3)
 b = engine.Value(4)
 c = a + b
 print(a, b, c)
-print(c.backward())
-d = engine.value(5)
+c.backward()
+print(c.grad, a.grad, b.grad)
+d = engine.Value(5)
 e = c + d
+print(e.get_all_children())
 print(e.backward())
+print(e.grad, d.grad, c.grad, a.grad, b.grad)
