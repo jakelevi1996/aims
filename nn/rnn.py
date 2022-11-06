51b90e8b90da4fc6f4c6c003a2762434494ae1cf
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-11-06 18:37:25 +0000

    Add get_params and zero_grad methods, and make _initial_hidden_state a vector of zeros

--- nn/rnn.py
+++ nn/rnn.py
@@ -10,7 +10,6 @@ class CharRnn:
         hidden_dim=None,
         encoder_mlp=None,
         decoder_mlp=None,
-        initial_hidden_state_tensor=None,
         rng=None,
     ):
         self._char_list = char_list
@@ -23,18 +22,16 @@ class CharRnn:
             rng = np.random.default_rng(0)
 
         if hidden_dim is None:
-            if initial_hidden_state_tensor is not None:
-                hidden_dim = initial_hidden_state_tensor.numel()
-            else:
-                hidden_dim = 5 * len(char_list)
+            hidden_dim = 5 * len(char_list)
         if encoder_mlp is None:
             encoder_mlp = nn.Mlp(
                 input_dim=(len(char_list) + hidden_dim),
                 output_dim=hidden_dim,
                 hidden_dim=hidden_dim,
                 num_hidden_layers=2,
-                output_act=nn.activation.linear,
+                output_act=nn.activation.gaussian,
                 hidden_act=nn.activation.relu,
+                rng=rng,
             )
         if decoder_mlp is None:
             decoder_mlp = nn.Mlp(
@@ -44,40 +41,46 @@ class CharRnn:
                 num_hidden_layers=1,
                 output_act=nn.activation.linear,
                 hidden_act=nn.activation.relu,
-            )
-        if initial_hidden_state_tensor is None:
-            initial_hidden_state_tensor = torch.tensor(
-                data=rng.random(size=[1, hidden_dim]),
-                dtype=torch.float32,
-                requires_grad=True,
+                rng=rng,
             )
 
         self._encoder_mlp = encoder_mlp
         self._decoder_mlp = decoder_mlp
-        self._initial_hidden_state = initial_hidden_state_tensor
+        self._initial_hidden_state = torch.zeros(
+            size=[1, hidden_dim],
+            dtype=torch.float32,
+        )
         self._char_vector = torch.zeros(
             size=[1, len(char_list)],
             dtype=torch.float32,
         )
+        self._hidden_state = None
 
     def consume(self, s):
         loss = 0
-        hidden_state = self._initial_hidden_state.detach().requires_grad_()
+        self._hidden_state = self._initial_hidden_state
         char_one_hot = self._get_char_one_hot(s[0])
         for c in s[1:]:
             rnn_input = torch.concatenate(
-                [char_one_hot, hidden_state],
+                [char_one_hot, self._hidden_state],
                 axis=1,
             )
-            hidden_state = self._encoder_mlp.forward(rnn_input)
-            rnn_output = self._decoder_mlp.forward(hidden_state)
+            self._hidden_state = self._encoder_mlp.forward(rnn_input)
+            rnn_output = self._decoder_mlp.forward(self._hidden_state)
             loss += nn.loss.cross_entropy_loss(
                 rnn_output,
                 [self._get_char_id(c)],
             )
             char_one_hot = self._get_char_one_hot(c)
 
-        return loss
+        return loss / len(s)
+
+    def get_params(self):
+        return self._encoder_mlp.get_params() + self._decoder_mlp.get_params()
+
+    def zero_grad(self):
+        self._encoder_mlp.zero_grad()
+        self._decoder_mlp.zero_grad()
 
     def _get_char_id(self, c):
         return self._char_dict.get(c, self._default_char_id)
